d5aacabc6b10d2dc906453e49733e799b050c1ec
abhijith-413 <abhijith511@outlook.com>
2023-11-12 12:40:18 +0530

    stack adt complete

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
-# data-structures-0.1
-basic data structures
+#implimenting stack data structures using arrays:
 
-solving data structure problems is fun
+1. create a class named array_stack
+2. initialize a constructor with a self object named data (list)
+3. create 2 additional methods to count the number of elements and to check if the
+array stack is empty
+4. code a method to push elements into the stack
+5. code a method to view the top element in a stack
--- stack.py
+++ stack.py
@@ -1,4 +1,31 @@
-class Node:
-    def __init__(self, data):
-        self.data = data
-        self.next = next
+# stack using arrays
+
+class ArrayStack:
+    def __init__(self):
+        self.data = []
+    def isEmpty(self):
+        return len(self.data) == 0
+    def len(self):
+        return len(self.data)
+    def push(self, ele):
+        self.data.append(ele)
+        print("element pushed")
+    def top(self):
+        if self.isEmpty == False:
+            self.data[-1]
+        else:
+            print("Stack empty")
+    def pop(self):
+        if self.isEmpty == False:
+            self.data.pop()
+        else:
+            print("Stack empty")
+    
+
+my_stack = ArrayStack()
+my_stack.push("orange")
+my_stack.push("500")
+my_stack.push("armour")
+
+my_stack.pop()
+my_stack.pop()
